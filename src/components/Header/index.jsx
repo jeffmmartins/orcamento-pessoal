4cc9b099e6b843a857b7cb0218058fc3ce19bfb5
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-05-11 22:30:26 -0300

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
-  )
-}
+    <header className="bg-success-subtle">
+      <nav className=" container navbar navbar-expand-lg">
+        <a href="">
+          <img src="img/money.png" alt="Dinheiro" />
+        </a>
+        <div className="ms-4">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="" className="nav-link fs-5">
+                Cadastro
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link fs-5">
+                Consulta
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default index;
