e5971474372d36224d8940cb519f60dc0a0b8745
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-05-18 18:35:16 -0300

    setup router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// configurando o router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Cadastro from "./routes/Cadastro";
+import Consulta from "./routes/Consulta";
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Cadastro/>
+  },
+  {
+    path:"consulta",
+    element: <Consulta/>
+  }
+])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
---
+++ src/routes/Consulta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Consulta = () => {
+  return (
+    <div>
+      <h1>teste</h1>
+    </div>
+  )
+}
+
+export default Consulta
