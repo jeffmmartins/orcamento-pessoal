ab3080d0373d565be1b417a59170cdbb8b8e4bdd
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-25 09:35:53 -0300

    persistencia de dados

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,73 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-  const {register, handleSubmit} = useForm();
-  const [form, setForm] = useState([]);
-  console.log("1", form)
+  const { register, handleSubmit } = useForm();
+  const dados = localStorage.getItem("cadastro");
+  const [form, setForm] = useState(dados ? JSON.parse(dados) : []);
 
+  // Atualização
   useEffect(() => {
-    const dados = localStorage.getItem('cadastro');
-    if (dados) {
-      setForm(JSON.parse(dados)); 
-    } else {
-      setForm([]); 
-    }
-  }, []);
- 
+    localStorage.setItem("cadastro", JSON.stringify(form));
+  }, [form]);
 
-  useEffect(() => {
-    localStorage.setItem("cadastro", JSON.stringify(form)); 
-  },[form]);
-  
   const salvarFormulario = (formdata) => {
-    //setForm([...form, formdata])
-    const dadosExistentes = localStorage.getItem('cadastro');
-    let dados = [];
-    if (dadosExistentes) {
-      dados = JSON.parse(dadosExistentes);
-    }
-    setForm([...dados, formdata]); 
-    localStorage.setItem("cadastro", JSON.stringify([...dados, formdata])); 
-  }
+    setForm([...form, formdata]);
+  };
 
   return (
     <div>
-      <form  onSubmit={handleSubmit(salvarFormulario)} >
+      <form onSubmit={handleSubmit(salvarFormulario)}>
         <select name="" id="">
-            <option value="">Ano</option>
-            <option value="2024" {...register("ano")}>2024</option>
+          <option value="">Ano</option>
+          <option value="2024" {...register("ano")}>
+            2024
+          </option>
         </select>
         <select name="" id="" {...register("mês")}>
-            <option value="">Mês</option>
-            <option value="1">Janeiro</option>
-            <option value="2">Fevereiro</option>
-            <option value="3">Março</option>
-            <option value="4">Abril</option>
-            <option value="5">Maio</option>
-            <option value="6">Junho</option>
-            <option value="7">Julho</option>
-            <option value="8">Agosto</option>
-            <option value="9">Setembro</option>
-            <option value="10">Outubro</option>
-            <option value="11">Novembro</option>
-            <option value="12">Dezembro</option>
+          <option value="">Mês</option>
+          <option value="1">Janeiro</option>
+          <option value="2">Fevereiro</option>
+          <option value="3">Março</option>
+          <option value="4">Abril</option>
+          <option value="5">Maio</option>
+          <option value="6">Junho</option>
+          <option value="7">Julho</option>
+          <option value="8">Agosto</option>
+          <option value="9">Setembro</option>
+          <option value="10">Outubro</option>
+          <option value="11">Novembro</option>
+          <option value="12">Dezembro</option>
         </select>
-        <input type="text" placeholder='Dia' {...register("dia")}/>
+        <input type="text" placeholder="Dia" {...register("dia")} />
         <select name="" id="" {...register("tipo")}>
-            <option value="">Tipo</option>
-            <option value="1">Alimentação</option>
-            <option value="2">Educação</option>
-            <option value="3">Lazer</option>
-            <option value="4">Saúde</option>
-            <option value="5">Transporte</option>
+          <option value="">Tipo</option>
+          <option value="1">Alimentação</option>
+          <option value="2">Educação</option>
+          <option value="3">Lazer</option>
+          <option value="4">Saúde</option>
+          <option value="5">Transporte</option>
         </select>
-        <input type="text"  placeholder='Descrição' {...register("descrição")}/>
-        <input type="text"  placeholder='Valor' {...register("valor")}/>
+        <input type="text" placeholder="Descrição" {...register("descrição")} />
+        <input type="text" placeholder="Valor" {...register("valor")} />
         <button>Adicionar</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
